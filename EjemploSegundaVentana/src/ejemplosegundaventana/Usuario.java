e0c30ac2681ecb245a1af2ffa911eaa9d5eda4c0
ingluisgarcia <ingluisgarciac@gmail.com>
2026-04-10 15:40:34 -0500

    First commit

---
+++ EjemploSegundaVentana/src/ejemplosegundaventana/Usuario.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplosegundaventana;
+
+/**
+ *
+ * @author cymaniatico
+ */
+public class Usuario {
+
+    private int idUser;
+    private String user;
+    private String pass;
+    private int idProfile;
+
+    public Usuario(int idUser, String user, String pass, int idProfile) {
+        this.idUser = idUser;
+        this.user = user;
+        this.pass = pass;
+        this.idProfile = idProfile;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public int getIdProfile() {
+        return idProfile;
+    }
+    
+    
+    
+}
